4e35fa5f5eade2d0acc45c43b01eeea76fac443c
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2021-07-12 13:38:27 +0900

    implementing checkForMatch and flipCard function

--- app.js
+++ app.js
@@ -22,19 +22,58 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "images/hotdog.png",
     },
   ];
+
+  const grid = document.querySelector(".grid");
+  const resultDisplay = document.querySelector("#result");
+  var cardsChosen = [];
+  var cardsChosenId = [];
+  var cardsWon = [];
+
+  //creating the board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement("img");
+      card.setAttribute("src", "images/blank.png");
+      card.setAttribute("data-id", i);
+      //card.addEventListener("click", flipCard);
+      grid.appendChild(card);
+    }
+  }
+
+  //check for matchs of the card
+  function checkForMatch() {
+    var cards = document.querySelectorAll("img");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+    if (cardsChosen[0] === cardsChosen[1]) {
+      alert("match found");
+      cards[optionOneId].setAttribute("src", "images/white.png");
+      cards[optionTwoId].setAttribute("src", "images/white.png");
+      cardsWon.push(cardsChosen);
+    } else {
+      cards[optionOneId].setAttribute("src", "images/blank.png");
+      cards[optionTwoId].setAttribute("src", "images/blank.png");
+      alert("sorry, try again!");
+    }
+    //making the arrays empty again
+    cardsChosen = [];
+    cardsChosenId = [];
+
+    resultDisplay;
+  }
+
+  //flip your card
+  function flipCard() {
+    var cardId = this.getAttribute("data-id");
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute("src", cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+    }
+  }
+
+  createBoard();
 });
 
-const grid = document.querySelector(".grid");
-
-//creating the board
-function createBoard() {
-  for (let i = 0; i < cardArray.lenght; i++) {
-    var card = document.createElement("img");
-    card.setAttribute("src", "images/blank.png");
-    card.setAttribute("data-id", i);
-    card.addEventListener("click", flipcard);
-    grid.appendChild(card);
-  }
-}
-
-createBoard();
+console.log("hello");
