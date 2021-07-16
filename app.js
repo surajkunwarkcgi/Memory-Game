1a6944ce71a8851bda87cd6fbeb8dd57b4754571
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2021-07-16 22:04:02 +0900

    Implemented resultDisplay and made the game work

--- app.js
+++ app.js
@@ -18,10 +18,40 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "images/cheeseburger.png",
     },
     {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+    {
       name: "hotdog",
       img: "images/hotdog.png",
     },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "ice-cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
   ];
+
+  cardArray.sort(() => 0.5 - Math.random());
 
   const grid = document.querySelector(".grid");
   const resultDisplay = document.querySelector("#result");
@@ -35,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
       var card = document.createElement("img");
       card.setAttribute("src", "images/blank.png");
       card.setAttribute("data-id", i);
-      //card.addEventListener("click", flipCard);
+      card.addEventListener("click", flipCard);
       grid.appendChild(card);
     }
   }
@@ -46,20 +76,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const optionOneId = cardsChosenId[0];
     const optionTwoId = cardsChosenId[1];
     if (cardsChosen[0] === cardsChosen[1]) {
-      alert("match found");
+      alert("Match found!");
       cards[optionOneId].setAttribute("src", "images/white.png");
       cards[optionTwoId].setAttribute("src", "images/white.png");
       cardsWon.push(cardsChosen);
     } else {
       cards[optionOneId].setAttribute("src", "images/blank.png");
       cards[optionTwoId].setAttribute("src", "images/blank.png");
-      alert("sorry, try again!");
+      alert("Sorry, try again!");
     }
     //making the arrays empty again
     cardsChosen = [];
     cardsChosenId = [];
 
-    resultDisplay;
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cardArray.length / 2) {
+      resultDisplay.textContent = "Congratulations! you found them all!";
+    }
   }
 
   //flip your card
@@ -75,5 +108,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   createBoard();
 });
-
-console.log("hello");
